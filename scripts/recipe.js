8ad0c730e9e9eddfea897c5eacbcb07e6c6ce13f
Claudio Campos <claudio@claudiocampos.com>
2013-12-22 10:45:27 -0500

    parsePreparation: WIP

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -1,4 +1,51 @@
-var ingredients = [tomatoes, salt];
+$(document).ready(function() {
+    var time1, time2, result;
 
-var ingredients = "2 pounds skinless, boneless chicken thighs $
-1/2 teaspoon freshly ground black pepper"
+    parseIngredients = function() {
+        var ingredientsUI = $("li[itemprop='ingredient']"),
+            ingredientsCount = ingredientsUI.length,
+            thisIngredientUI = null,
+            ingredients = [],
+            i = 0;
+
+        for (; i < ingredientsCount; i += 1) {
+            thisIngredientUI = ingredientsUI[i];
+            amount = thisIngredientUI.children[0].innerHTML;
+            name = thisIngredientUI.children[1].innerHTML;
+
+            thisIngredient = {}
+            thisIngredient.name = name;
+            thisIngredient.amount = amount;
+            ingredients.push(thisIngredient);
+        }
+
+        return ingredients;
+    };
+
+    parsePreparation = function() {
+        var preparationStepsUI = $("ol[itemprop='instructions']")[0].children,
+            preparationStepsCount = preparationStepsUI.length,
+            thisStepUI = null,
+            preparationSteps = [],
+            subArr,
+            i = 0,
+            j = 0;
+
+        for (; i < preparationStepsCount; i += 1) {
+            thisStepUI = preparationStepsUI[i].innerHTML;
+            subArr = thisStepUI.split('.');
+            lastIndex = subArr.length - 1;
+            firstSubstep = subArr[0];
+            lastSubstep = subArr[lastIndex];
+            if (typeof +firstSubstep.substring(0, 1) === 'number') {
+                subArr.shift();
+            }
+            if (lastSubstep === "") {
+                subArr.pop();
+            }
+            console.log(subArr);
+        }
+    };
+
+    parsePreparation();
+});
